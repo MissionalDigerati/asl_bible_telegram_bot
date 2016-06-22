496ff02e660b8e2fec9ea2b696236df1baae2ad1
Codemis <johnathan@jpulos.com>
2016-06-22 13:38:01 -0700

    Added the Restify library for web hooks.

--- config.example.js
+++ config.example.js
@@ -3,6 +3,14 @@
  *
  */
 var config = {};
+/**
+ * Do you want to poll Telegram, or user the server endpoits.
+ * @type {Boolean}
+ */
+config.usePolling = false;
+config.serverPort = 3030;
+config.serverDomain = 'http://localhost:3030';
+config.publicKeyCertificatePath = '';
 config.telegram = {};
 config.digitalBiblePlatform = {};
 config.telegram.botToken = 'MY-TELEGRAM-BOT-TOKEN';
